e5977cb75f5a4b9abbe86003301734ed1d0fe800
patty <pattycheng2008@gmail.com>
2022-02-01 21:28:06 +0800

    add product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
 
   def index
+    @ad = {
+      title: "廣告",
+      description: "描述"
+    }
+    @products = []
+
+    (1..100).each do |index|
+      product = {
+        id: index,
+        name: "馬卡龍",
+        description: "好吃",
+        image_url: "https://images.pexels.com/photos/239581/pexels-photo-239581.jpeg"
+      }
+      @products << product
+    end 
   end
   
   def test
